b849fcbd4fab320167766d12a49f278804da5a60
Artur Tamm <tamm3@llnl.gov>
2018-05-22 14:59:57 -0700

    Small change on the electronic heat test

--- Tests/EPH_FDM/Laser/test.cpp
+++ Tests/EPH_FDM/Laser/test.cpp
@@ -10,7 +10,7 @@
  */
 
 // create an electronic system
-EPH_FDM electrons {10000, 1, 1};
+EPH_FDM electrons {1000000, 1, 1};
 
 void laser_hit() {
   
@@ -18,7 +18,18 @@ void laser_hit() {
 
 int main(int args, char **argv) {
   // do the MPI_Init
+  electrons.setBox( // in Ang
+    -3000.0, 3000.0,
+    0.0, 28.16, // 8 x 3.52 current shock simulation
+    0.0, 28.16
+    );
+  electrons.setConstants(300.0, 3.5e-6, 1.0, 0.1248); // T_e, C_e, rho_e, kappa_e
+  electrons.setSteps(1); // minimum nr. of steps
+  electrons.setDt(0.0001); // in ps
   
+  char a {'a'};
+  while(a != 'e')
+    std::cin >> a;
   
   // do the MPI_Finalise
   
