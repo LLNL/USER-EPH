13078750f57119403342f69fba21f2f2ae47bfde
Artur Tamm <tamm3@llnl.gov>
2018-07-05 10:50:29 -0700

    Modified the sync file before 0.9.9

--- sync.sh
+++ sync.sh
@@ -10,8 +10,18 @@ git pull
 
 echo "Syncing files in private repo with public repo"
 SRC=("README.md" "LICENSE" "eph_spline.h" "eph_spline.cpp" "eph_beta.h" "eph_beta.cpp" "eph_fdm.h" "eph_fdm.cpp" "fix_eph.h" "fix_eph.cpp")
+SRC=(${SRC[*]} "Doc/Beta/input.beta" "Doc/FDM/T_input.fdm")
+
+# documentation on input files
 SRC=(${SRC[*]} "Examples/Beta/Ni.beta" "Examples/Beta/Ni_model_4.beta")
+# Example 1
 SRC=(${SRC[*]} "Examples/Example_1/Ni.eam" "Examples/Example_1/Ni_model_4.beta" "Examples/Example_1/run.lmp" "Examples/Example_1/README" "Examples/Example_1/Tout.pdf" "Examples/Example_1/Tout.png")
+# Example 2
+SRC=(${SRC[*]} "Examples/Example_2/Ni.eam" "Examples/Example_2/Ni_model_4.beta" "Examples/Example_2/run.lmp" "Examples/Example_2/README" "Examples/Example_2/Tout.pdf" "Examples/Example_2/Tout.png")
+# Example 3
+SRC=(${SRC[*]} "Examples/Example_3/Ni.eam" "Examples/Example_3/Ni_model_4.beta" "Examples/Example_3/run.lmp" "Examples/Example_3/README" "Examples/Example_3/Tout.pdf" "Examples/Example_3/Tout.png")
+# Example 4
+SRC=(${SRC[*]} "Examples/Example_4/Ni.eam" "Examples/Example_4/Ni_model_4.beta" "Examples/Example_4/run.lmp" "Examples/Example_4/T.in" "Examples/Example_4/README" "Examples/Example_4/Tfieldout.pdf" "Examples/Example_4/Tfieldout.png")
 
 N=${#SRC[*]}
 echo $N
