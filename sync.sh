d4feaf6add9e0982a38721e7e523d9b6564899bb
Artur Tamm <tamm3@llnl.gov>
2018-07-05 09:08:34 -0700

    Working on examples

--- sync.sh
+++ sync.sh
@@ -9,7 +9,9 @@ cd $OUT
 git pull
 
 echo "Syncing files in private repo with public repo"
-SRC=("README.md" "LICENSE" "eph_spline.h" "eph_spline.cpp" "eph_beta.h" "eph_beta.cpp" "eph_fdm.h" "eph_fdm.cpp" "fix_eph.h" "fix_eph.cpp" "Examples/Beta/Ni.beta" "Examples/Beta/Ni_model_4.beta")
+SRC=("README.md" "LICENSE" "eph_spline.h" "eph_spline.cpp" "eph_beta.h" "eph_beta.cpp" "eph_fdm.h" "eph_fdm.cpp" "fix_eph.h" "fix_eph.cpp")
+SRC=(${SRC[*]} "Examples/Beta/Ni.beta" "Examples/Beta/Ni_model_4.beta")
+SRC=(${SRC[*]} "Examples/Example_1/Ni.eam" "Examples/Example_1/Ni_model_4.beta" "Examples/Example_1/run.lmp" "Examples/Example_1/README" "Examples/Example_1/Tout.pdf" "Examples/Example_1/Tout.png")
 
 N=${#SRC[*]}
 echo $N
@@ -27,6 +29,7 @@ for i in $(seq 0 $[N-1]) ; do
 
   done
 
+cd $OUT
 git commit
 git push
 
