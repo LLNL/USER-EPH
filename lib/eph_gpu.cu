e9b3f42bf8fa051cf6309cddb06af8ade1ec4854
Artur Tamm <artur.tamm.85@gmail.com>
2019-08-24 04:24:15 +0000

    gpu interpolator

--- lib/eph_gpu.cu
+++ lib/eph_gpu.cu
@@ -3,6 +3,8 @@
 #include <cuda_runtime_api.h>
 #include <stdio.h>
 
+#include "eph_gpu.h"
+
 __global__
 void dummy_test(int myID, int nrPS)
 {
@@ -13,15 +15,35 @@ void dummy_test(int myID, int nrPS)
   printf("GPU %d %d %d %d %d\n", myID, nrPS, thread_index, block_dimension, grid_dimension);
 }
 
+void run_dummy_test_cu(int myID, int nrPS) 
+{
+  dummy_test<<<1, 1>>>(myID, nrPS);
+}
+
+__global__ 
+void test_interpolator_cu(EPH_Spline_GPU spl, double *values, int n_values)
+{
+  int thread_index = threadIdx.x;
+  int block_dimension = blockDim.x;
+  int grid_dimension = gridDim.x;
+
+  spl(5.0);
+}
+
+void test_interpolator_gpu(EPH_Spline_GPU spl, double *values, int n_values) {
+  test_interpolator_cu<<<1, 1>>>(spl, values, n_values);
+}
+
 __global__
 void calculate_environment_gpu_cu()
 {
   
 }
 
-
-void run_dummy_test_cu(int myID, int nrPS) 
+void calculate_environment_gpu()
 {
-  dummy_test<<<1, 1>>>(myID, nrPS);
+  
 }
+
+
 
