4f24edb0390c5b9ea99620589b89534abcdd5b7c
Artur Tamm <artur.tamm.work@gmail.com>
2021-03-31 22:12:17 +0000

    Added utility for creating kappa file

--- utils/kappa/create.m
+++ utils/kappa/create.m
@@ -19,7 +19,7 @@ N_r = 1001; # number of points for table
 dr = r_max / (N_r - 1);
 r = [0.0:dr:r_max];
 
-E_max = 10.0; # maximum energy in eV
+E_max = 1.0; # maximum energy in eV
 N_E = 1001; # number of points for energy
 dE = E_max / (N_E - 1);
 E = [0.0:dE:E_max];
@@ -38,7 +38,7 @@ for i = [1:1:N]
   data = load(fn);
   r_in = data(:, 1);
   rho_in = data(:, 2);
-  rho_interp = interp1(r_in, rho_in, r, "pchip", 0.0);
+  rho_interp = interp1(r_in, rho_in, r, "pchip", "extrap");
   rho_r(i, :) = rho_interp(:);
 endfor
 ### END RHO_R ###
@@ -48,19 +48,27 @@ for i = [1:1:N]
   fn = sprintf("data/energy_%d.data", i);
   data = load(fn);
   T_in = data(:, 1);
-  E_in = data(:, 2);
+  E_in = data(:, 2) / N_atoms(i);
   E_shift = E_in(1);
 
-  T_E_interp = interp1(E_in - E_shift, T_in, E, "pchip", 0.0);
+  T_E_interp = interp1(E_in - E_shift, T_in, E, "pchip", "extrap");
   T_E(i, :) = T_E_interp(:);
 endfor
 ### END T(E) ###
 
 ### KAPPA(E) ###
+k = 1;
 for i = [1:1:N]
   for j = [i:1:N]
-    fn = sprintf("data/energy_%d_%d.data", i, j);
+    fn = sprintf("data/kappa_%d_%d.data", i, j);
     data = load(fn);
+    E_in = data(:, 1) / kB;
+    K_in = data(:, 2);
+    
+    K_E_interp = interp1(E_in, K_in, E, "pchip", "extrap");
+    K_E(k, :) = K_E_interp(:);
+
+    k = k + 1;
   endfor
 endfor
 ### END KAPPA(E) ###
