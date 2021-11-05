74f6c8a0ac9353e138092c3339323c6d3d114158
Artur Tamm <artur.tamm.work@gmail.com>
2021-11-05 07:08:31 +0000

    y(x) and x(y) mapping is working

--- eph_linear.h
+++ eph_linear.h
@@ -6,7 +6,13 @@
 #ifndef EPH_LINEAR
 #define EPH_LINEAR
 
+#include <stddef.h> // bring size_t into scope
+
 #include <vector>
+#include <iterator> // std::distance
+#include <algorithm> // std::lower_bound
+
+#include <iostream>
 
 /*
  * This is a linear interpolation that supports reverse lookup.
@@ -14,16 +20,46 @@
  */
 
 struct EPH_Linear {
-  std::vector<double> x;
+  double dx;
   std::vector<double> y;
-  
+  std::vector<double> dy;
   
   EPH_Linear() = default;
   
-  template<typename x_it, typename y_it>
-  EPH_Linear(x_it x_begin, x_it x_end, y_it y_begin) {
+  template<typename y_it>
+  EPH_Linear(double _dx, y_it y_begin, y_it y_end)
+      : dx {_dx} {
+    while(y_begin != y_end) {
+      y.push_back(*(y_begin++));
+    }
     
+    for(size_t i = 0; i < y.size() - 1; ++i) {
+      dy.push_back((y[i+1] - y[i]) / dx);
+    }
+  }
+  
+  // given an x find the y value
+  double operator() (double _x) { 
+    size_t idx = static_cast<size_t>(_x / dx); // we ignore the end point
+    if(idx < y.size()) {
+      double delta = _x - idx * dx;
+      return y[idx] + dy[idx] * delta;
+    }
+    return 0.;
+  }
+  
+  // given a y find the appropriate x
+  double reverse_lookup(double _y) {
+    auto it {std::upper_bound(y.begin(), y.end(), _y)};
+    if(it != y.end()) {
+      size_t idx {static_cast<size_t>(std::distance(y.begin(), it))};
+      idx--;
+      return idx * dx + 1. / dy[idx] * (_y - y[idx]);
+    }
+    
+    return 0.;
   }
   
 };
 
+#endif
